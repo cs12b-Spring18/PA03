81ace92124661a01a4d82354b5d27e0ee9c6f428
richardwchz <33837558+richardwchz@users.noreply.github.com>
2018-02-11 23:00:35 -0500

    改好啦

--- RichardCircle.java
+++ RichardCircle.java
@@ -1,5 +1,8 @@
-package pa03team17;
+package circleshape;
+
 import java.awt.*;
+import java.awt.event.*;
+
 
 /**
  * RadiusChangingCircles are yellow circles which move at a constant velocity
@@ -7,27 +10,28 @@ import java.awt.*;
 */
 public class RichardCircle extends CircleShape{
 
-  private double vr=1.0;
-  private int frameNumber;
+  private double vr=1.0;    //adding a new instance variable
+  public int num = 0;
 
 	/**
 	 * create a radius changing circle and set the color to yellow
 	 */
-/**  public RadiusChangingCircle(){
-    super();
-		this.color = new java.awt.Color(0,0,255,100); // transparent blue
-  }
-*/
-  public void draw(Graphics g){
-    int x = frameNumber;
-    int y = 500-frameNumber;
+  public RichardCircle(){
+    super();      //the constructor of the circle shape.
+		this.color = new java.awt.Color(255,255,255,255); // transparent blue
+  }       //changes the color
 
-    if ((frameNumber/2)%2 == 0){
-      x = frameNumber%500;
-      y = 500 - frameNumber%500;
+
+  public void draw(Graphics g){
+    int x = num;
+    int y = 500-num;
+
+    if ((num/2)%2 == 0){
+      x = num%500;
+      y = 500 - num%500;
     }else{
-      x = 500 - frameNumber%500;
-      y = frameNumber%500;
+      x = 500 - num%500;
+      y = num%500;
     }
 
     int u = (int)(250-0.5*x);
@@ -39,19 +43,20 @@ public class RichardCircle extends CircleShape{
     g.setColor( Color.GREEN );
     g.drawOval( u,v,w,h );
   }
+
+
 	/**
 	 * update the circle as usual, but also change the radius. If it gets too big,
 	 * then start it shrinking, if it gets too small then start it growing.
 	 */
   public void update(double dt){
     // change the properties of the CircleShape after dt seconds have elapsed.
-
-
-		this.radius += dt*vr;
+    num++;
+		this.radius += dt*vr;  //changed raduis based on dt and vr
 
     if  (this.radius <10) this.vr *= -1;
     else if (this.radius > 50) this.vr *= -1;
-		super.update(dt);
+		super.update(dt);  //calls the parent one
   }
 
 
